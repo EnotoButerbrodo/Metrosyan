c047044dec9534093af1390ff7b90e03bce0acf8
Vladimir99users <51012754+Vladimir99users@users.noreply.github.com>
2022-08-23 14:04:53 +1000

    Создание магии
    Создание големов через фабрику, добавил второй слот, что бы вытаскивать из него нужный материал.

--- Assets/GolemCrafter.cs
+++ Assets/GolemCrafter.cs
@@ -1,21 +1,27 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GolemCrafter : MonoBehaviour
 {
-    [SerializeField] private CoreSlot _typeSlot;
+    [SerializeField] private CoreSlot _mainTypeSlot;
+    [SerializeField] private CoreSlot _extraTypeSlot;
     [SerializeField] private CoreMenu _menu;
+    [SerializeField] private ExtraConfig defualtCore;
 
-    private AttackFactoryBase _attackFactory = new RangeAttackFactory();
-
+    
+    [SerializeField] private RegularSpells _factory;
+    private ElementType extraType = ElementType.Bullet;
 
     public void Craft()
     {
-        if (_typeSlot.Item is null)
+        if (_mainTypeSlot.Item is null )
             return;
 
-        IAttack golemAttack = _attackFactory.Get(_typeSlot.Item.Stats);
-        Spell golem = new Golem(golemAttack);
+        if(!(_extraTypeSlot.Item is null)) 
+        {
+            extraType = _extraTypeSlot.Item.Stats.Type;
+        }
+        
+        var golem = _factory.Get(_mainTypeSlot.Item.Stats.Type, extraType);
         golem.Use();
     }
 
